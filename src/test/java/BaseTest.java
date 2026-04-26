0a255235ca80d96e79dba6ee183acb7446eb5b06
Veit Vladimir <makoveev20122012@gmail.com>
2026-04-26 21:59:21 +0500

    Alert

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -9,12 +10,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.swing.*;
 import java.time.Duration;
 
 public class BaseTest {
 
     WebDriver driver;
     WebDriverWait wait;
+    Actions action;
 
     @BeforeMethod
     public void options() {
@@ -24,6 +27,7 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        action = new Actions(driver);
     }
 
 
---
+++ src/test/java/ContextMenuTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+public class ContextMenuTest extends BaseTest{
+
+    @Test
+    public void checkTextAlert() {
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+        action.contextClick(driver.findElement(By.id("hot-spot"))).perform();
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals(alert.getText(), "You selected a context menu");
+        alert.accept();
+    }
+}
